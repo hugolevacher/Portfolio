9399deee4d4719d4815cf099e44ba1404d3e6f3a
hugolevacher <hugolevacher13@gmail.com>
2025-03-19 17:44:31 -0400

    FCT: About me, skills and contact sections

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Portfolio';
+  scrollToSection(event: Event, sectionId: string): void {
+    event.preventDefault();
+    const element = document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }
 }
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,23 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  imports: [CommonModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+  skills = [
+    { name: 'C#', logo: 'assets/c-sharp-icon.svg' },
+    { name: 'Kotlin', logo: 'assets/kotlin-icon.svg' },
+    { name: 'JavaScript', logo: 'assets/javascript-icon.svg' },
+    { name: 'TypeScript', logo: 'assets/typescript-icon.svg' },
+    { name: 'HTML', logo: 'assets/html-icon.svg' },
+    { name: 'CSS', logo: 'assets/css-icon.svg' },
+    { name: 'SQL', logo: 'assets/sql-icon.svg' },
+    { name: 'Angular', logo: 'assets/angular-icon.svg' },
+    { name: '.NET', logo: 'assets/microsoft-dot-net-icon.svg' },
+    { name: 'Git', logo: 'assets/git-icon.svg' },
+  ];
+}
