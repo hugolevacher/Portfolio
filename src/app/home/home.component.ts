d7409eb03c228cebf2ea09b969f1fca638b617ee
hugolevacher <hugolevacher13@gmail.com>
2025-03-20 12:20:16 -0400

    FCT: Arrows to change home page section

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { DataService } from '../services/data.service';
 
@@ -12,11 +12,47 @@ import { DataService } from '../services/data.service';
 export class HomeComponent {
   skills: any = [];
   projects: any = [];
+  sections: HTMLElement[] = [];
+  currentSectionIndex: number = 0;
+  hasPreviousSection: boolean = false;
+  hasNextSection: boolean = true;
 
   constructor(private dataService: DataService) { }
 
   ngOnInit(): void {
     this.skills = this.dataService.skills;
     this.projects = this.dataService.projects;
+    this.sections = Array.from(document.querySelectorAll('section'));
+    this.updateArrowVisibility();
+  }
+
+  scrollToNextSection(): void {
+    const currentScroll = window.scrollY;
+    const nextSection = this.sections.find(section => section.offsetTop > currentScroll + 10);
+    if (nextSection) {
+      nextSection.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+
+  scrollToPreviousSection(): void {
+    const currentScroll = window.scrollY;
+    const previousSection = [...this.sections].reverse().find(section => section.offsetTop < currentScroll - 10);
+    if (previousSection) {
+      previousSection.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+
+  @HostListener('window:scroll', [])
+  onScroll(): void {
+    const currentScroll = window.scrollY + window.innerHeight / 2; // Middle of the viewport
+    this.currentSectionIndex = this.sections.findIndex(
+      section => section.offsetTop <= currentScroll && section.offsetTop + section.offsetHeight > currentScroll
+    );
+    this.updateArrowVisibility();
+  }
+
+  private updateArrowVisibility(): void {
+    this.hasPreviousSection = this.currentSectionIndex > 0;
+    this.hasNextSection = this.currentSectionIndex < this.sections.length - 1;
   }
 }
